99764135c026393347e4e000b0f976f2bd93d475
ItzMESs <111136989+ItzMESs@users.noreply.github.com>
2026-06-02 04:46:26 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');
   next();
